ae8a7326031eaec8af13f478a93dbb9ce446c761
Alan Edwardes <alan@alanedwardes.com>
2020-07-18 22:58:00 +0100

    Adds package.bat
    (cherry picked from commit 0850dffe10875d13857486d99baeb86fb49b1f33)

---
+++ package.bat
@@ -0,0 +1,6 @@
+dotnet pack src/Estranged.Lfs.Adapter.S3 -o packages
+dotnet pack src/Estranged.Lfs.Api -o packages
+dotnet pack src/Estranged.Lfs.Authenticator.BitBucket -o packages
+dotnet pack src/Estranged.Lfs.Authenticator.GitHub -o packages
+dotnet pack src/Estranged.Lfs.Data -o packages
+pause
