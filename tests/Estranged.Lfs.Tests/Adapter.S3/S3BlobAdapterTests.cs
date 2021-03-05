c3e64a80d1e0a0a2bf51e6cea41fe91c873892c4
Alan Edwardes <alan@alanedwardes.com>
2021-03-05 22:19:33 +0000

    Adds real unit tests for S3BlobAdapter.

--- tests/Estranged.Lfs.Tests/Adapter.S3/S3BlobAdapterTests.cs
+++ tests/Estranged.Lfs.Tests/Adapter.S3/S3BlobAdapterTests.cs
@@ -1,40 +1,52 @@
-﻿using Amazon.S3;
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Estranged.Lfs.Adapter.S3;
-using Estranged.Lfs.Data;
-using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Estranged.Lfs.Tests.Adapter.S3
 {
-    public class S3BlobAdapterTests
+    public class S3BlobAdapterTests : IDisposable
     {
-        private IBlobAdapter CreateAdapter()
-        {
-            var config = new S3BlobAdapterConfig
-            {
-            };
-            return new ServiceCollection().AddLfsS3Adapter(config, new AmazonS3Client()).BuildServiceProvider().GetRequiredService<IBlobAdapter>();
-        }
+        private readonly MockRepository mockRepository = new MockRepository(MockBehavior.Strict);
 
-        [Fact(Skip = "Should use mock data")]
+        public void Dispose() => mockRepository.VerifyAll();
+
+        [Fact]
         public async Task TestDownloadBlobFound()
         {
-            var adapter = CreateAdapter();
+            var mockClient = mockRepository.Create<IAmazonS3>();
+
+            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", CancellationToken.None))
+                      .ReturnsAsync(new GetObjectMetadataResponse{ContentLength = 19961});
+
+            mockClient.Setup(x => x.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>()))
+                      .Returns("https://www.example.com/");
+
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
 
             var signedBlob = await adapter.UriForDownload("d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", CancellationToken.None);
 
             Assert.Null(signedBlob.ErrorCode);
             Assert.Null(signedBlob.ErrorMessage);
             Assert.Equal(19961, signedBlob.Size);
-            Assert.NotNull(signedBlob.Uri);
+            Assert.Equal("https://www.example.com/", signedBlob.Uri.ToString());
         }
 
-        [Fact(Skip = "Should use mock data")]
+        [Fact]
         public async Task TestDownloadBlobNotFound()
         {
-            var adapter = CreateAdapter();
+            var mockClient = mockRepository.Create<IAmazonS3>();
+
+            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "wibble", CancellationToken.None))
+                      .ThrowsAsync(new AmazonS3Exception("Error making request with Error Code NotFound and Http Status Code NotFound. No further error information was returned by the service.", ErrorType.Sender, "NotFound", "requestId", HttpStatusCode.NotFound));
+
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
 
             var signedBlob = await adapter.UriForDownload("wibble", CancellationToken.None);
 
@@ -42,16 +54,21 @@ namespace Estranged.Lfs.Tests.Adapter.S3
             Assert.Equal("Error making request with Error Code NotFound and Http Status Code NotFound. No further error information was returned by the service.", signedBlob.ErrorMessage);
         }
 
-        [Fact(Skip = "Should use mock data")]
+        [Fact]
         public async Task TestUploadBlob()
         {
-            var adapter = CreateAdapter();
+            var mockClient = mockRepository.Create<IAmazonS3>();
+
+            mockClient.Setup(x => x.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>()))
+                      .Returns("https://www.example.com/");
+
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
 
             var signedBlob = await adapter.UriForUpload("wibble", 10, CancellationToken.None);
 
             Assert.Null(signedBlob.ErrorCode);
             Assert.Null(signedBlob.ErrorMessage);
-            Assert.NotNull(signedBlob.Uri);
+            Assert.Equal("https://www.example.com/", signedBlob.Uri.ToString());
         }
     }
 }
