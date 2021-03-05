891120bcd2e5bf2744b73fe07ef75d8a28d6018d
Alan Edwardes <alan@alanedwardes.com>
2021-03-05 22:27:22 +0000

    Improves unit tests for S3BlobAdapter.

--- tests/Estranged.Lfs.Tests/Adapter.S3/S3BlobAdapterTests.cs
+++ tests/Estranged.Lfs.Tests/Adapter.S3/S3BlobAdapterTests.cs
@@ -22,13 +22,20 @@ namespace Estranged.Lfs.Tests.Adapter.S3
         {
             var mockClient = mockRepository.Create<IAmazonS3>();
 
-            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", CancellationToken.None))
+            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "prefix/d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", CancellationToken.None))
                       .ReturnsAsync(new GetObjectMetadataResponse{ContentLength = 19961});
 
             mockClient.Setup(x => x.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>()))
+                      .Callback<GetPreSignedUrlRequest>(request =>
+                      {
+                          Assert.Equal("prefix/d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", request.Key);
+                          Assert.Equal("my-bucket", request.BucketName);
+                          Assert.Equal(HttpVerb.GET, request.Verb);
+                          Assert.Equal(Protocol.HTTPS, request.Protocol);
+                      })
                       .Returns("https://www.example.com/");
 
-            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket", KeyPrefix = "prefix/" });
 
             var signedBlob = await adapter.UriForDownload("d53de494a038b6a8ede0aea08c38bde00244b155924bf4c463d1de208faecee8", CancellationToken.None);
 
@@ -43,10 +50,10 @@ namespace Estranged.Lfs.Tests.Adapter.S3
         {
             var mockClient = mockRepository.Create<IAmazonS3>();
 
-            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "wibble", CancellationToken.None))
+            mockClient.Setup(x => x.GetObjectMetadataAsync("my-bucket", "prefix/wibble", CancellationToken.None))
                       .ThrowsAsync(new AmazonS3Exception("Error making request with Error Code NotFound and Http Status Code NotFound. No further error information was returned by the service.", ErrorType.Sender, "NotFound", "requestId", HttpStatusCode.NotFound));
 
-            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket", KeyPrefix = "prefix/" });
 
             var signedBlob = await adapter.UriForDownload("wibble", CancellationToken.None);
 
@@ -60,9 +67,16 @@ namespace Estranged.Lfs.Tests.Adapter.S3
             var mockClient = mockRepository.Create<IAmazonS3>();
 
             mockClient.Setup(x => x.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>()))
+                      .Callback<GetPreSignedUrlRequest>(request =>
+                      {
+                          Assert.Equal("prefix/wibble", request.Key);
+                          Assert.Equal("my-bucket", request.BucketName);
+                          Assert.Equal(HttpVerb.PUT, request.Verb);
+                          Assert.Equal(Protocol.HTTPS, request.Protocol);
+                      })
                       .Returns("https://www.example.com/");
 
-            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket" });
+            var adapter = new S3BlobAdapter(mockClient.Object, new S3BlobAdapterConfig { Bucket = "my-bucket", KeyPrefix = "prefix/" });
 
             var signedBlob = await adapter.UriForUpload("wibble", 10, CancellationToken.None);
 
