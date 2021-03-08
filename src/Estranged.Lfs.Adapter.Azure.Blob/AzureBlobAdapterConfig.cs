fde42e52ab4d6b6b0912747032d1a53646598b93
Stephan van Stekelenburg <stephan@hayman.io>
2021-03-08 23:21:30 +0100

    fix: remove some initial assignments

--- src/Estranged.Lfs.Adapter.Azure.Blob/AzureBlobAdapterConfig.cs
+++ src/Estranged.Lfs.Adapter.Azure.Blob/AzureBlobAdapterConfig.cs
@@ -6,9 +6,9 @@ namespace Estranged.Lfs.Adapter.Azure.Blob
     {
         public string ConnectionString { get; set; }
 
-        public string ContainerName { get; set; } = "gitlfs";
+        public string ContainerName { get; set; }
 
-        public string KeyPrefix { get; set; } = string.Empty;
+        public string KeyPrefix { get; set; }
 
         public TimeSpan Expiry { get; set; } = TimeSpan.FromHours(1);
     }
