338d3cc8623d5f05c007a2fe2be50a6383e96a53
Alan Edwardes <alan@alanedwardes.com>
2021-04-23 21:22:03 +0100

    Adds PublishLambda.ps1

---
+++ PublishLambda.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = "Stop"
+
+$OutputFolder = "hosting/Estranged.Lfs.Hosting.Lambda/bin/Release/netcoreapp3.1/linux-x64/publish"
+$OutputFile = "Estranged.Lfs.Hosting.Lambda.zip"
+$InputProject = "hosting/Estranged.Lfs.Hosting.Lambda/Estranged.Lfs.Hosting.Lambda.csproj"
+
+Remove-Item -LiteralPath $OutputFile -ErrorAction Ignore
+Remove-Item -LiteralPath $OutputFolder -Force -Recurse -ErrorAction Ignore
+dotnet publish $InputProject --runtime linux-x64 --configuration Release
+Compress-Archive -Path $OutputFolder/* -DestinationPath $OutputFile
