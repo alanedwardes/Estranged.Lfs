56e893b0d826bedd011326c2bf4571af1bc9e2b4
Alan Edwardes <alan@alanedwardes.com>
2021-04-23 21:42:02 +0100

    Corrects PublishLambda.ps1

--- PublishLambda.ps1
+++ PublishLambda.ps1
@@ -5,6 +5,14 @@ $OutputFile = "Estranged.Lfs.Hosting.Lambda.zip"
 $InputProject = "hosting/Estranged.Lfs.Hosting.Lambda/Estranged.Lfs.Hosting.Lambda.csproj"
 
 Remove-Item -LiteralPath $OutputFile -ErrorAction Ignore
+
 Remove-Item -LiteralPath $OutputFolder -Force -Recurse -ErrorAction Ignore
-dotnet publish $InputProject --runtime linux-x64 --configuration Release
+
+dotnet publish $InputProject `
+    --configuration "Release" `
+    --framework "netcoreapp3.1" `
+    /p:GenerateRuntimeConfigurationFiles=true `
+    --runtime linux-x64 `
+    --self-contained false
+
 Compress-Archive -Path $OutputFolder/* -DestinationPath $OutputFile
