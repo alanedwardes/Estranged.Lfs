b1ae243cdf3897280058da69bf0fb3693ac669b6
Alan Edwardes <alan@alanedwardes.com>
2026-07-12 23:27:22 +0100

    Update PublishLambda.ps1

--- PublishLambda.ps1
+++ PublishLambda.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = "Stop"
 
-$OutputFolder = "hosting/Estranged.Lfs.Hosting.Lambda/bin/Release/net8.0/linux-x64/publish"
+$OutputFolder = "hosting/Estranged.Lfs.Hosting.Lambda/bin/Release/net10.0/linux-x64/publish"
 $OutputFile = "Estranged.Lfs.Hosting.Lambda.zip"
 $InputProject = "hosting/Estranged.Lfs.Hosting.Lambda/Estranged.Lfs.Hosting.Lambda.csproj"
 
@@ -10,7 +10,7 @@ Remove-Item -LiteralPath $OutputFolder -Force -Recurse -ErrorAction Ignore
 
 dotnet publish $InputProject `
     --configuration "Release" `
-    --framework "net8.0" `
+    --framework "net10.0" `
     /p:GenerateRuntimeConfigurationFiles=true `
     --runtime linux-x64 `
     --self-contained false
